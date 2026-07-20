cc7042508ec952b9c4da5bc67c4096653ad6771b
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-21 00:49:32 +0600

    AppBar 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,58 @@ void main(){
       home: Scaffold(
         //for cover my screen
         backgroundColor: Colors.deepPurple,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent
+                    ),
+                    child:Text(
+                      "My Menu",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30
+                      ),
+                    )
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Profile"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.account_box_outlined),
+                title: Text("About"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout"),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           centerTitle: true,
           toolbarHeight: 64,
           elevation: 10,
           backgroundColor: Colors.blueGrey,
-          leading: Icon(Icons.menu_book, color: Colors.white,),
+          //leading: Icon(Icons.menu_book, color: Colors.white,),
           title: Text("AppBar", style: TextStyle(
             color: Colors.white,
             letterSpacing: 3,
@@ -27,7 +73,9 @@ void main(){
                 icon: Icon(Icons.search, color: Colors.white,)
             ),
           ],
+
         ),
+
         body: Card(),
       ),
     ),
