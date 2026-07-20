da40adc077facb5efc1dd3150ffa49c451d514a0
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-21 00:08:57 +0600

    Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,33 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      //debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         //for cover my screen
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          centerTitle: true,
+          toolbarHeight: 64,
+          elevation: 10,
+          backgroundColor: Colors.blueGrey,
+          leading: Icon(Icons.menu_book, color: Colors.white,),
+          title: Text("AppBar", style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 3,
+            fontSize: 35,
+            fontWeight: FontWeight.bold
+          ),),
+          actions: [
+            IconButton(onPressed: () {}, 
+                icon: Icon(Icons.notifications, color: Colors.white,)
+            ),
+            IconButton(onPressed: () {},
+                icon: Icon(Icons.search, color: Colors.white,)
+            ),
+          ],
+        ),
+        body: Card(),
       ),
-      color: Colors.green,
     ),
   );
 }
