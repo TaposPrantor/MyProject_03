36773a8d6c9f93df9849dabc2960251692a8b948
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-20 23:21:23 +0600

    Fisrt Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,18 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main(){
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("My App"),),
-        body: Center(child: Text("Hello",),),
+      home: Text(
+        "First Text Run By Flutter Code",
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+          fontSize: 35,
+        ),
       ),
-    );
-  }
+    )
+  );
 }
