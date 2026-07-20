d8f3cb0ea7a0fdf066752d79be5bcabfa3b28f99
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-20 23:36:41 +0600

    RunApp, MatrialApp, Scafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Text(
-        "First Text Run By Flutter Code",
-        style: TextStyle(
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-          fontSize: 35,
-        ),
+      //debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        //for cover my screen
       ),
-    )
+      color: Colors.green,
+    ),
   );
 }
