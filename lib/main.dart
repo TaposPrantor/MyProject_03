fd804f3ad6643da44f4771d185f3154521dbafb5
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-22 11:49:18 +0600

    Border 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,91 +7,6 @@ void main() {
       home: Scaffold(
         //for cover my screen
         //backgroundColor: Colors.deepPurple,
-        endDrawer: Drawer(
-          child: ListView(
-            children: [
-              SizedBox(
-                height: 100,
-                child: DrawerHeader(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vic2l0ZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D"
-                        ),
-                      fit: BoxFit.cover
-                    )
-                  ),
-                    child: Text("Menu Item?Bar", style: TextStyle(fontSize: 23, color: Colors.red),)
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Profile"),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text("Home"),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.account_box_outlined),
-                  title: Text("About"),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text("Setting"),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.info_outline),
-                  title: Text("Information"),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.logout),
-                  title: Text("Log Out"),
-                  onTap: () {},
-                ),
-              )
-
-            ],
-          ),
-        ),
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.all(8),
@@ -142,11 +57,75 @@ void main() {
             ],
           ),
         ),
+        endDrawer: Drawer(
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vic2l0ZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D"
+                            ),
+                            fit: BoxFit.cover
+                        )
+                    ),
+                    child: Text("Menu Item?Bar", style: TextStyle(fontSize: 23, color: Colors.red),)
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("Profile"),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home"),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.account_box_outlined),
+                  title: Text("About"),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Setting"),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.info_outline),
+                  title: Text("Information"),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text("Log Out"),
+                  onTap: () {},
+                ),
+              )
+
+            ],
+          ),
+        ),
         appBar: AppBar(
           centerTitle: true,
           toolbarHeight: 64,
           elevation: 10,
           backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.green,
           //leading: Icon(Icons.menu_book, color: Colors.white,),
           title: Text(
             "AppBar",
@@ -160,17 +139,25 @@ void main() {
 
         ),
         body: Center(
-          child: Container(
-            height: 200,
+          child: Center(
+            child: Container(
+              height: 200,
               width: 350,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              border: Border(
-                top: BorderSide(color: Colors.greenAccent, width: 3),
-                right: BorderSide(color: Colors.grey, width: 3),
-                bottom: BorderSide(color: Colors.green, width: 3),
-                left: BorderSide(color: Colors.purple, width: 3)
-              )
+              decoration: BoxDecoration(
+                color: Colors.red,
+                border: Border(
+                  top: BorderSide(color: Colors.greenAccent, width: 6),
+                  right: BorderSide(color: Colors.grey, width: 6),
+                  bottom: BorderSide(color: Colors.green, width: 6),
+                  left: BorderSide(color: Colors.purple, width: 6),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
             ),
           ),
         ),
