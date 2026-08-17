6015d3c00e94878eb34df642027e011c7616e344
taposroy306919-collab <taposroy306919@gmail.com>
2026-08-17 18:25:38 +0600

    App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,16 @@ void main(){
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My Project 02"),
+          title: Text(
+            "My Project 02",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepOrange
+            ),
+          ),
+          backgroundColor: Colors.orange.shade50,
+          toolbarHeight: 50,
         ),
       ),
     )
