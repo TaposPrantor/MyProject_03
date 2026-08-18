d8730595af24ac4e9cee7deefee89c7cb838e5ad
taposroy306919-collab <taposroy306919@gmail.com>
2026-08-18 23:05:27 +0600

    Colors Shade

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ void main(){
           child: Container(
             height: 40,
             width: 180,
-            color: Colors.grey,
+            color: Colors.grey.shade200,
             child: Center(
               child: Text(
                 "Tapos Roy",
